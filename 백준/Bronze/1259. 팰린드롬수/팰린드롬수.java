371227dd7d8cb7f2a6e3bd4a29314c570e73a108
박민수 <68321360+minsoo0506@users.noreply.github.com>
2024-01-05 15:41:12 +0900

    [Bronze I] Title: 팰린드롬수, Time: 120 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            boolean flag = true;
+
+            if(str.equals("0")){
+                break;
+            }
+
+            for(int i = 0; i < str.length() / 2; i++){
+                if(str.charAt(i) != str.charAt(str.length() - 1 - i)){
+                    flag = false;
+                }
+            }
+
+            if(flag == true){
+                sb.append("yes\n");
+            }
+            else{
+                sb.append("no\n");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
