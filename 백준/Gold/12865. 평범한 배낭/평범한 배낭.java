5474a533b4d0d02ebbe1c22a35baec72464abbe3
박민수 <68321360+minsoo0506@users.noreply.github.com>
2024-03-18 16:20:27 +0900

    Update 평범한 배낭.java

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -22,14 +22,19 @@ public class Main {
         }
 
         int max = 0;
+        // 물건을 1개부터 N개까지 넣어보기
         for(int i = 1; i <= N; i++){
+            // 무게 제한을 1부터 K까지 늘려가기
             for(int j = 1; j <= K; j++){
                 if(w[i - 1] <= j){
+                    // 배낭에 물건을 넣는 것과 넣지 않는 것 중에 무게 j를 넘지 않으면서 최대인 가치 구하기
                     dp[i][j] = Math.max(v[i - 1] + dp[i - 1][j - w[i - 1]], dp[i - 1][j]);
                 } else {
+                    // 물건을 넣었을 때 무게 j를 초과하는 경우
                     dp[i][j] = dp[i - 1][j];
                 }
 
+                // 최대 가치 값 갱신
                 max = Math.max(max, dp[i][j]);
             }
         }
