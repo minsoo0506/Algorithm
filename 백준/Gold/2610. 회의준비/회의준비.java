5d5ddd7cefced885f87dc4b387729479d3add7be
박민수 <68321360+minsoo0506@users.noreply.github.com>
2024-04-03 17:58:31 +0900

    Update 회의준비.java

--- 백준/Gold/2610. 회의준비/회의준비.java
+++ 백준/Gold/2610. 회의준비/회의준비.java
@@ -2,15 +2,14 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     private static final int INF = 100_000_000;
     private static int N, M;
     private static int[][] dist;
-    
+    private static boolean[] visited;
+
     // 플로이드-와샬 알고리즘
     public static void floydWarshall(){
         for(int k = 1; k <= N; k++){
@@ -21,6 +20,57 @@ public class Main {
                 }
             }
         }
+    }
+
+    public static int findLeader(int start){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(start);
+
+        // BFS
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+
+            for(int i = 1; i <= N; i++){
+                if(dist[now][i] != INF && !visited[i]){
+                    visited[i] = true;
+                    queue.offer(i);
+                    arrayList.add(i);
+                }
+            }
+        }
+
+        // 플로이드-와샬 알고리즘 수행
+        floydWarshall();
+
+        // 의사 전달 시간의 최대값이 최소인 값 찾기
+        int index = -1;
+        int minCost = INF;
+
+        for(int i = 1; i <= N; i++){
+            if(!arrayList.contains(i)){
+                continue;
+            }
+
+            int maxCost = 0;
+            for(int j = 1; j <= N; j++){
+                if(!arrayList.contains(j)){
+                    continue;
+                }
+                
+                maxCost = Math.max(maxCost, dist[i][j]);
+            }
+
+            if(minCost > maxCost){
+                minCost = maxCost;
+                index = i;
+            }
+        }
+
+        return index;
     }
 
     public static void main(String[] args) throws IOException {
@@ -46,55 +96,22 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
+            // 양방향 인접 행렬
             dist[a][b] = dist[b][a] = 1;
         }
 
-        floydWarshall();
-
-        ArrayList<Integer> reps = new ArrayList<>();
-        boolean[] visited = new boolean[N + 1];
-
-        // 각 노드에서 가장 먼 노드와의 거리가 가장 짧은 노드를 대표로 설정
-
-        // 모든 노드 순회
-        for (int i = 1; i <= N; i++) {
-            // 방문하지 않은 노드만 고려 : 새로운 위원회의 대표가 될 수 있음
-            if (!visited[i]) {
-                // minIdx는 현재 위원회의 대표 후보
-                // minVal은 해당 대표 후보에서 가장 먼 노드와의 거리
-                int minIdx = i;
-                int minVal = INF;
-                // i와 연결된 노드 찾기
-                for (int j = 1; j <= N; j++) {
-                    // i와 j가 연결되어 있으면 j를 방문처리
-                    if (dist[i][j] != INF) {
-                        visited[j] = true;
-                        int maxVal = 0;
-                        // j에서 가장 먼 노드와 거리 계산
-                        for (int k = 1; k <= N; k++) {
-                            if (dist[j][k] != INF) {
-                                maxVal = Math.max(maxVal, dist[j][k]);
-                            }
-                        }
-                        /*
-                        j에서 가장 먼 노드와의 거리가 현재 대표 후보에서 가장 먼 노드와의 거리보다
-                        짧으면, j를 새로운 대표 후보로 선정
-                        */
-                        if (minVal > maxVal) {
-                            minVal = maxVal;
-                            minIdx = j;
-                        }
-                    }
-                }
-                // 모든 노드를 순회한 후, 최종 대표 후보를 위원회의 대표로 선정
-                reps.add(minIdx);
+        visited = new boolean[N + 1];
+        ArrayList<Integer> leaders = new ArrayList<>();
+        for(int i = 1; i <= N; i++){
+            if(!visited[i]){
+                leaders.add(findLeader(i));
             }
         }
 
-        Collections.sort(reps);
-        System.out.println(reps.size());
-        for (int rep : reps) {
-            System.out.println(rep);
+        Collections.sort(leaders);
+        System.out.println(leaders.size());
+        for (int leader : leaders) {
+            System.out.println(leader);
         }
     }
 }
