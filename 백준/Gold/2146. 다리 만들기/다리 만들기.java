98c1c170ef7f91a62eec1a9c25d9dadadafa4384
박민수 <68321360+minsoo0506@users.noreply.github.com>
2024-05-07 15:12:47 +0900

    Update 다리 만들기.java

--- 백준/Gold/2146. 다리 만들기/다리 만들기.java
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.java
@@ -1,3 +1,5 @@
+// 백준 2146 다리 만들기
+// BFS와 DFS 사용하여 풀이
 import java.io.*;
 import java.util.*;
 
@@ -18,13 +20,17 @@ public class Main {
     private static int[][] map;
     private static boolean[][] visit;
     private static int minBridgeLength;
+
+    // 우,상,좌,하
     private static int[] dx = {1, 0, -1, 0};
     private static int[] dy = {0, 1, 0, -1};
 
+    // 새로운 x, y 좌표가 이차원 배열 내에 위치하는지 확인하는 메소드
     public static boolean rangeCheck(int x, int y){
         return x >= 0 && x < N && y >= 0 && y < N;
     }
 
+    // 각각의 좌표가 섬(1)인지 확인 후, 좌표에 섬의 번호 부여
     public static void dfs(int x, int y, int id){
         visit[x][y] = true;
         map[x][y] = id;
@@ -44,6 +50,7 @@ public class Main {
 
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
+                // (i, j)가 id번 째 섬에 해당한다면 큐에 저장
                 if(map[i][j] == id){
                     visit[i][j] = true;
                     queue.offer(new Point(i, j, 0));
@@ -60,9 +67,13 @@ public class Main {
                 int ny = cur.y + dy[i];
 
                 if(rangeCheck(nx, ny)){
+                    // 이동한 좌표가 육지이면서 다른 번호를 가진 섬이라면,
+                    // 해당 섬까지의 거리와 minlength를 비교하여 더 작은 값을 저장
                     if(map[nx][ny] != 0 && map[nx][ny] != id){
                         minlength = Math.min(minlength, cur.cost);
-                    } else if(map[nx][ny] == 0 && !visit[nx][ny]){
+                    } 
+                    // 이동한 좌표가 바다라면 이전까지의 cost에 1을 더하여 큐에 저장
+                    else if(map[nx][ny] == 0 && !visit[nx][ny]){
                         visit[nx][ny] = true;
                         queue.offer(new Point(nx, ny, cur.cost + 1));
                     }
@@ -74,6 +85,7 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
+        // 사용자 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
@@ -86,6 +98,7 @@ public class Main {
             }
         }
 
+        // map[i][j]가 1이라면 섬의 번호(id) 부여(저장)
         int id = 1;
         for(int i = 0; i < N; i++){
             for(int j = 0; j < N; j++){
